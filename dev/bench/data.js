202fb48ce380cf43c24544370e9aeb6168dc094d
github-action-benchmark <github@users.noreply.github.com>
2024-06-26 06:12:47 +0000

    add Arm Cortex-A72 (Raspberry Pi 4) benchmarks (customSmallerIsBetter) benchmark result for 1a574dd5151ea55836ef558ff4c46b93ee973afc

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1719382367618,
+  "repoUrl": "https://github.com/potsrevennil/mlkem-c-aarch64",
+  "entries": {
+    "Arm Cortex-A72 (Raspberry Pi 4) benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "15379156+potsrevennil@users.noreply.github.com",
+            "name": "Thing-han, Lim",
+            "username": "potsrevennil"
+          },
+          "committer": {
+            "email": "15379156+potsrevennil@users.noreply.github.com",
+            "name": "Thing-han, Lim",
+            "username": "potsrevennil"
+          },
+          "distinct": true,
+          "id": "1a574dd5151ea55836ef558ff4c46b93ee973afc",
+          "message": "test",
+          "timestamp": "2024-06-26T14:11:09+08:00",
+          "tree_id": "f9215fd819e567666b27d5da4cc8ef0f65e7367e",
+          "url": "https://github.com/potsrevennil/mlkem-c-aarch64/commit/1a574dd5151ea55836ef558ff4c46b93ee973afc"
+        },
+        "date": 1719382366765,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
+            "unit": "Percent"
+          }
+        ]
+      }
+    ]
+  }
+}
